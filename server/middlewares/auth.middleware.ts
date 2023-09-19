2226284b6aa264133477bcdfaf1b57105f2ee467
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-09-19 08:42:31 +0500

    server: auth middleware complete

--- server/middlewares/auth.middleware.ts
+++ server/middlewares/auth.middleware.ts
@@ -1,12 +1,14 @@
-import {NextFunction, Response, Request} from "express";
+import {NextFunction, Response} from "express";
 import {tokenParser} from "../helpers/tokenGenerator";
 import {User} from "../models/user.model";
+import {ExpressRequest} from "../types/express";
 
-export default async function(req: Request, res: Response, next: NextFunction){
+export default async function(req: ExpressRequest, res: Response, next: NextFunction){
     const token = req.headers['x-token']
     if(!token || typeof token !== "string") return res.status(401).send({code: 401, message: "Must have a token"})
     const {email} = tokenParser(token)
     const user = await User.findOne({email})
-    // req.user = user
+    if(!user) return res.status(401).send({code: 401, message: "Auth verification is failed"})
+    req.user = user
     next()
 }
