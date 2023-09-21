3ef97250b0ea027a911aab57d05db1b68c078725
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-09-21 11:48:00 +0500

    client: credit card component

---
+++ client/app/components/CreditCard/index.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+
+function CreditCard({ children }: { children?: ReactNode }) {
+  return (
+    <div className="block m-3 max-w-sm w-[400px] h-[250px] p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      {children ? (
+        children
+      ) : (
+        <div className="text-center pt-3 cursor-pointer">
+          <i className="fa-solid fa-plus text-9xl dark:text-white text-gray-900"></i>
+          <p className="dark:text-white text-gray-900 text-sm">
+            Yangi karta qo'shing
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default CreditCard;
---
+++ client/app/components/CreditCardInfo/index.tsx
@@ -0,0 +1,16 @@
+function CreditCardInfo() {
+  return (
+    <>
+      <h2 className="dark:text-white text-gray-900">Karta</h2>
+      <i className="fa-regular fa-credit-card text-3xl dark:text-white text-gray-900 mt-3"></i>
+      <h2 className="text-2xl font-extrabold dark:text-white my-5">
+        0000 0000 0000 0000
+      </h2>
+      <p className="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">
+        Sanjarbek Ismatov
+      </p>
+    </>
+  );
+}
+
+export default CreditCardInfo;
