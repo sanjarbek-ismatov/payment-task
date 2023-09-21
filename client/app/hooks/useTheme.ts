cff65abc792c3e372ecc8369a4f8d2ed49d10388
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-09-21 10:55:42 +0500

    client: toggle theme

---
+++ client/app/hooks/useTheme.ts
@@ -0,0 +1,19 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const useTheme = () => {
+  const [theme, setTheme] = useState("");
+  useEffect(() => {
+    const localTheme = localStorage.getItem("theme");
+    if (!localTheme) localStorage.setItem("theme", "light");
+    setTheme(localTheme ?? "light");
+  }, []);
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+  return [theme, toggleTheme] as [string, () => void];
+};
+export { useTheme };
