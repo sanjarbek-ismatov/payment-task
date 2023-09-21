dfb6109b615c895e4bf435b4adbc309ebdb1bb5d
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-09-21 10:38:22 +0500

    client: main page

---
+++ client/app/components/Navbar/index.tsx
@@ -0,0 +1,86 @@
+import DropdownWithAvatar from "@/app/components/DropdownWithAvatar";
+import GradientButton from "@/app/components/GradientButton";
+import SideBar from "@/app/components/SideBar";
+import ToggleTheme from "../ToggleTheme";
+
+function LogoutIcon() {
+  return (
+    <svg
+      className="w-6 h-6 text-gray-800 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 18 16"
+    >
+      <path
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
+      ></path>
+    </svg>
+  );
+}
+function Navbar() {
+  return (
+    <>
+      <nav className="bg-white fixed top-0 z-50 w-full border-b dark:border-b-gray-800 dark:bg-gray-900">
+        <div className=" flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="https://flowbite.com/" className="flex items-center">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8 mr-3"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Payment
+            </span>
+          </a>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex items-center flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <GradientButton>Pul o'tkazish</GradientButton>
+              </li>
+              <li>
+                <ToggleTheme />
+              </li>
+              <li>
+                <DropdownWithAvatar />
+              </li>
+              <li>
+                <LogoutIcon />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <SideBar />
+    </>
+  );
+}
+export default Navbar;
