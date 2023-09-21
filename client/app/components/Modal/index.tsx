41d9557f74bba9d62c8d635931bdb57411b6f634
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-09-21 14:42:26 +0500

    client: modal component details

--- client/app/components/Modal/index.tsx
+++ client/app/components/Modal/index.tsx
@@ -46,81 +46,53 @@ function Modal({
             </button>
             <div className="px-6 py-6 lg:px-8">
               <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
-                Sign in to our platform
+                Yangi karta malumotlarini kiriting
               </h3>
               <form className="space-y-6" action="#">
                 <div>
                   <label
-                    htmlFor="email"
+                    htmlFor="number"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Your email
+                    Karta raqami
                   </label>
                   <input
-                    type="email"
-                    name="email"
-                    id="email"
+                    type="number"
+                    name="cardNumber"
+                    id="number"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    placeholder="name@company.com"
-                    required={true}
+                    placeholder="0000 0000 0000 0000"
+                    required
                   />
                 </div>
                 <div>
                   <label
-                    htmlFor="password"
+                    htmlFor="cvv"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Your password
+                    CVV
                   </label>
                   <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
+                    type="number"
+                    name="cvv"
+                    id="cvv"
+                    placeholder="123"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    required={true}
+                    required
                   />
                 </div>
-                <div className="flex justify-between">
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="remember"
-                        type="checkbox"
-                        defaultValue=""
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                        required={true}
-                      />
-                    </div>
-                    <label
-                      htmlFor="remember"
-                      className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >
-                      Remember me
-                    </label>
-                  </div>
-                  <a
-                    href="#"
-                    className="text-sm text-blue-700 hover:underline dark:text-blue-500"
-                  >
-                    Lost Password?
-                  </a>
-                </div>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  type="date"
+                  name="expirationDate"
+                  required
+                />
                 <button
                   type="submit"
                   className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  Login to your account
+                  Kartani qo'shish
                 </button>
-                <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                  Not registered?{" "}
-                  <a
-                    href="#"
-                    className="text-blue-700 hover:underline dark:text-blue-500"
-                  >
-                    Create account
-                  </a>
-                </div>
               </form>
             </div>
           </div>
