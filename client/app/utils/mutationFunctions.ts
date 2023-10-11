f0c0cf0ebd98a2f6984300b7492ec6e1d5ae7a56
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-10-11 18:45:10 +0500

    content type

--- client/app/utils/mutationFunctions.ts
+++ client/app/utils/mutationFunctions.ts
@@ -5,62 +5,47 @@ import { Mutation, ServerResponse } from "../types";
 import { getToken } from "./getToken";
 import { QueryClient, UseMutationResult } from "react-query";
 
-export  const mutationFunc = (url: string, method: string, withToken: boolean) => {
-    return async (body: BodyInit) => {
-      const token = getToken();
-    
-      const res = await fetch(url, {
-        method,
-        body,
-        ...(withToken && {
-          headers: {
-            ["x-token"]: token || "",
-            ["Content-Type"]: body instanceof FormData ? "multipart/form-data" : "application/json"
-          },
-        }),
-      
-      });
-      try{
-         const data = await res.json();
-         if (res.ok) return data as ServerResponse;
-         return Promise.reject(data.message);
-      } catch(ex){
-          return {} as ServerResponse
-      }
-    };
-  };
+export const mutationFunc = (
+  url: string,
+  method: string,
+  withToken: boolean
+) => {
+  return async (body: BodyInit) => {
+    const token = getToken();
 
-  export const formSubmit = (mutation: Mutation, queryClient: QueryClient, ...queries: string[]) =>  {
-    return async function (event: FormEvent) {
-      event.preventDefault();
-      toast.promise(
-        async () => {
-          const promise = mutation.mutateAsync(
-            new FormData(event.currentTarget as HTMLFormElement),
-            {
-              onSuccess() {
-                queries.forEach((query) => {
-                  queryClient.invalidateQueries(query);
-                });
-              },
-            }
-          );
-          return promise;
+    const res = await fetch(url, {
+      method,
+      body,
+      ...(withToken && {
+        headers: {
+          ["x-token"]: token || "",
+          ...(!(body instanceof FormData) && {
+            ["Content-Type"]: "application/json",
+          }),
         },
-        {
-          success: "Bajarildi",
-          error: "Nimadir xato ketdi",
-          pending: "Bajarilmoqda...",
-        },
-        toastOptions
-      );
-    };
-  }
-  export const submitData = (mutation: Mutation, queryClient: QueryClient, body: any, ...queries: string[]) => {
+      }),
+    });
+    try {
+      const data = await res.json();
+      if (res.ok) return data as ServerResponse;
+      return Promise.reject(data.message);
+    } catch (ex) {
+      return {} as ServerResponse;
+    }
+  };
+};
+
+export const formSubmit = (
+  mutation: Mutation,
+  queryClient: QueryClient,
+  ...queries: string[]
+) => {
+  return async function (event: FormEvent) {
+    event.preventDefault();
     toast.promise(
       async () => {
         const promise = mutation.mutateAsync(
-          JSON.stringify(body),
+          new FormData(event.currentTarget as HTMLFormElement),
           {
             onSuccess() {
               queries.forEach((query) => {
@@ -78,4 +63,30 @@ export  const mutationFunc = (url: string, method: string, withToken: boolean) =
       },
       toastOptions
     );
-  }
+  };
+};
+export const submitData = (
+  mutation: Mutation,
+  queryClient: QueryClient,
+  body: any,
+  ...queries: string[]
+) => {
+  toast.promise(
+    async () => {
+      const promise = mutation.mutateAsync(JSON.stringify(body), {
+        onSuccess() {
+          queries.forEach((query) => {
+            queryClient.invalidateQueries(query);
+          });
+        },
+      });
+      return promise;
+    },
+    {
+      success: "Bajarildi",
+      error: "Nimadir xato ketdi",
+      pending: "Bajarilmoqda...",
+    },
+    toastOptions
+  );
+};
