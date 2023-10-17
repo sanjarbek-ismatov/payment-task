5928684b0650a2009c53e220b41412233d08d92a
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-10-17 14:48:35 +0500

    getting user from server got updated

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -1,45 +1,65 @@
-import express from 'express'
-import {upload} from "../models/gridfs.model";
+import express from "express";
+import { upload } from "../models/gridfs.model";
 import userValidator from "../helpers/validators/user";
-import {User} from "../models/user.model";
-import {passwordChecker, passwordGenerator} from "../helpers/passwordManager";
-import {tokenGenerator} from "../helpers/tokenGenerator";
-import authMiddleware from '../middlewares/auth.middleware';
-import { ExpressRequest } from '../types/express';
-const router = express.Router()
-router.post("/signup", upload.single('image'), async (req, res) => {
-    const {error} = userValidator.registerValidator(req.body)
-    if(error) return res.status(400).send({code: 400, message: error.details[0].message})
-    const checkEmail = await User.findOne({email: req.body.email})
-    if(checkEmail) return res.status(403).send({code: 403, message: "The email is already available"})
-    const newUser = new User(req.body)
-    if(req.file){
-        newUser.image = req.file.filename
-    }
-    newUser.password = await passwordGenerator(req.body.password)
-    await newUser.save()
-    res.status(201).send({code: 201, message: "User has been created"})
-})
-router.post('/signing', upload.none(), async (req, res) => {
-    const {error} = userValidator.loginValidator(req.body)
-    const {email, password} = req.body
-    if(error) return res.status(400).send({code: 400, message: error.details[0].message})
-    const user = await User.findOne({email})
-    if(!user) return res.status(404).send({code: 404, message: "User is not found"})
-    const arePasswordsTheSame = await passwordChecker(password, user.password)
-    if(!arePasswordsTheSame) return res.status(400).send({code: 400, message: "The password is wrong"})
-    const token = tokenGenerator(email, "email")
-    res.setHeader('x-token', token).status(200).send({code: 200, message: "Login complete"})
-})
-router.get('/me', authMiddleware, async (req: ExpressRequest, res) => {
-    const user = req.user
-    res.status(200).send({code: 200, result: await user?.populate("cards payments")})
-})
-router.get('/:email', async (req, res) => {
-    const {email} = req.params
-    const user = await User.findOne({email}).select("-password -payments").populate("cards")
-    if(!user) return res.status(404).send({code: 404, message: "User is not found"})
-    return res.status(200).send({code: 200, result: user})
-})
+import { User } from "../models/user.model";
+import { passwordChecker, passwordGenerator } from "../helpers/passwordManager";
+import { tokenGenerator } from "../helpers/tokenGenerator";
+import authMiddleware from "../middlewares/auth.middleware";
+import { ExpressRequest } from "../types/express";
+const router = express.Router();
+router.post("/signup", upload.single("image"), async (req, res) => {
+  const { error } = userValidator.registerValidator(req.body);
+  if (error)
+    return res
+      .status(400)
+      .send({ code: 400, message: error.details[0].message });
+  const checkEmail = await User.findOne({ email: req.body.email });
+  if (checkEmail)
+    return res
+      .status(403)
+      .send({ code: 403, message: "The email is already available" });
+  const newUser = new User(req.body);
+  if (req.file) {
+    newUser.image = req.file.filename;
+  }
+  newUser.password = await passwordGenerator(req.body.password);
+  await newUser.save();
+  res.status(201).send({ code: 201, message: "User has been created" });
+});
+router.post("/signing", upload.none(), async (req, res) => {
+  const { error } = userValidator.loginValidator(req.body);
+  const { email, password } = req.body;
+  if (error)
+    return res
+      .status(400)
+      .send({ code: 400, message: error.details[0].message });
+  const user = await User.findOne({ email });
+  if (!user)
+    return res.status(404).send({ code: 404, message: "User is not found" });
+  const arePasswordsTheSame = await passwordChecker(password, user.password);
+  if (!arePasswordsTheSame)
+    return res
+      .status(400)
+      .send({ code: 400, message: "The password is wrong" });
+  const token = tokenGenerator(email, "email");
+  res
+    .setHeader("x-token", token)
+    .status(200)
+    .send({ code: 200, message: "Login complete" });
+});
+router.get("/me", authMiddleware, async (req: ExpressRequest, res) => {
+  const user = req.user;
+  res
+    .status(200)
+    .send({ code: 200, result: await user?.populate("cards payments") });
+});
+router.get("/", async (req, res) => {
+  const user = await User.findOne(req.body)
+    .select("-password -payments")
+    .populate("cards");
+  if (!user)
+    return res.status(404).send({ code: 404, message: "User is not found" });
+  return res.status(200).send({ code: 200, result: user });
+});
 
-export default router
+export default router;
