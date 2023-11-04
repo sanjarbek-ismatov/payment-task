1c60b3b80f990202a60ff3d057bcde6321463907
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-11-04 20:09:16 +0500

    client: utils to render dates

---
+++ client/app/utils/dateToRead.ts
@@ -0,0 +1,29 @@
+export const convertIndexToMonth = (index: number) => {
+  const months = [
+    "Yanvar",
+    "Fevral",
+    "Mart",
+    "Aprel",
+    "May",
+    "Iyun",
+    "Iyul",
+    "Avgust",
+    "Sentabr",
+    "Oktabr",
+    "Noyabr",
+    "Dekabr",
+  ];
+  return months[index];
+};
+export const convertDayToWeekDay = (index: number) => {
+  const daysOfTheWeek = [
+    "Dushanba",
+    "Seshanba",
+    "Chorshanba",
+    "Payshanba",
+    "Juma",
+    "Shanba",
+    "Yakshanba",
+  ];
+  return daysOfTheWeek[index];
+};
