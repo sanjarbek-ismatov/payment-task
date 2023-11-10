0b741aed6b5b872b0662a3e91b7149b8bf88b5b6
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-11-10 13:32:27 +0500

    client: Setting page is being created

--- client/app/settings/components/BasicUserDetails/index.tsx
+++ client/app/settings/components/BasicUserDetails/index.tsx
@@ -1,8 +1,29 @@
 import ProfileImageChanger from "@/app/settings/components/ProfileImageChanger";
+import DefaultInput from "@/app/components/DefaultInput";
+import { UserInterface } from "@/app/types";
+import Card from "@/app/components/Card";
 
-function BasicUserDetails(){
-    return <form>
-        <ProfileImageChanger />
-    </form>
+function BasicUserDetails({ details }: { details: UserInterface }) {
+  return (
+    <Card>
+      <form className="flex w-[800px] items-end">
+        <ProfileImageChanger src={details.image} />
+        <div className="flex-1 m-3">
+          <DefaultInput
+            name="fullName"
+            type="text"
+            defaultValue={details.fullName}
+          />
+          <DefaultInput
+            name="birthday"
+            type="date"
+            defaultValue={new Date(details.birthday)
+              .toISOString()
+              .substring(0, 10)}
+          />
+        </div>
+      </form>
+    </Card>
+  );
 }
-export default BasicUserDetails
+export default BasicUserDetails;
--- client/app/settings/page.tsx
+++ client/app/settings/page.tsx
@@ -1,8 +1,20 @@
+"use client";
 import BasicUserDetails from "@/app/settings/components/BasicUserDetails";
+import { useQuery } from "react-query";
+import { userInfoQuery } from "@/app/utils/queryFunctions";
+import H2 from "@/app/components/H2";
 
-function SettingsPage(){
-    return <>
-        <BasicUserDetails />
-    </>
+function SettingsPage() {
+  const { data } = useQuery("user", userInfoQuery);
+  return (
+    <div className='p-4 text-center'>
+        <H2>Sozlamalar</H2>
+      {data && data.result ? (
+        <BasicUserDetails details={data.result} />
+      ) : (
+        <p></p>
+      )}
+    </div>
+  );
 }
-export default SettingsPage
+export default SettingsPage;
