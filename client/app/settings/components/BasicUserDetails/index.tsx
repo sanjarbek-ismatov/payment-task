922710b06c5d63896b57584c21f3ee9f36319912
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-11-10 20:37:47 +0500

    client: Setting page is in progress

--- client/app/settings/components/BasicUserDetails/index.tsx
+++ client/app/settings/components/BasicUserDetails/index.tsx
@@ -2,27 +2,38 @@ import ProfileImageChanger from "@/app/settings/components/ProfileImageChanger";
 import DefaultInput from "@/app/components/DefaultInput";
 import { UserInterface } from "@/app/types";
 import Card from "@/app/components/Card";
+import SettingCard from "@/app/settings/components/SettingCard";
 
 function BasicUserDetails({ details }: { details: UserInterface }) {
   return (
-    <Card>
-      <form className="flex w-[800px] items-end">
+    <Card style={{ margin: "20px auto" }}>
+     <SettingCard>
         <ProfileImageChanger src={details.image} />
         <div className="flex-1 m-3">
+          <div className='flex w-full'>
           <DefaultInput
             name="fullName"
             type="text"
+            label="Ismingiz"
             defaultValue={details.fullName}
           />
           <DefaultInput
             name="birthday"
             type="date"
+            label="Tug'ilgan sanangiz"
             defaultValue={new Date(details.birthday)
               .toISOString()
               .substring(0, 10)}
           />
+          </div>
+          <DefaultInput
+            name="country"
+            label="Mamlakat"
+            type='text'
+            defaultValue={details.country}
+          />
         </div>
-      </form>
+     </SettingCard>
     </Card>
   );
 }
--- client/app/settings/page.tsx
+++ client/app/settings/page.tsx
@@ -3,14 +3,37 @@ import BasicUserDetails from "@/app/settings/components/BasicUserDetails";
 import { useQuery } from "react-query";
 import { userInfoQuery } from "@/app/utils/queryFunctions";
 import H2 from "@/app/components/H2";
+import Card from "@/app/components/Card";
+import DefaultInput from "@/app/components/DefaultInput";
+import { UserInterface } from "@/app/types";
+import SettingCard from "@/app/settings/components/SettingCard";
+
+function ContactSettings({ details }: { details: UserInterface }) {
+  return (
+    <Card>
+      <SettingCard>
+        <DefaultInput
+          name="phone"
+          label="Telefon raqami"
+          defaultValue={details.phone}
+        />
+        <DefaultInput name="email" label="Email" defaultValue={details.email} />
+      </SettingCard>
+    </Card>
+  );
+}
 
 function SettingsPage() {
   const { data } = useQuery("user", userInfoQuery);
   return (
-    <div className='p-4 text-center'>
-        <H2>Sozlamalar</H2>
+    <div className="p-4 text-center">
+      <H2>Sozlamalar</H2>
       {data && data.result ? (
-        <BasicUserDetails details={data.result} />
+        <form>
+          <BasicUserDetails details={data.result} />
+          <br />
+          <ContactSettings details={data.result} />
+        </form>
       ) : (
         <p></p>
       )}
