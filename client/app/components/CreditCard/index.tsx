62a0bdbc9789c5f44c8d15d7af54b2d3f214b5db
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2023-11-27 13:20:10 +0500

    client: CreditCard component changes

--- client/app/components/CreditCard/index.tsx
+++ client/app/components/CreditCard/index.tsx
@@ -1,33 +1,33 @@
-import { ComponentProps, ReactNode } from "react";
+import {ComponentProps} from "react";
 
 function CreditCard({
-  children,
-  selected,
-  ...rest
-}: ComponentProps<"div"> & { selected?: boolean }) {
-  return (
-    <div
-      className={`${
-        selected ? "border-green-500" : "border-transparent"
-      } border-2 rounded md:inline-block block`}
-    >
-      <div
-        {...rest}
-        className="group cursor-pointer block m-3 max-w-sm md:w-[400px] w-full md:h-[250px] h-[300px] p-6 relative bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-      >
-        {children ? (
-          children
-        ) : (
-          <div className="text-center pt-3 cursor-pointer">
-            <i className="fa-solid fa-plus text-9xl dark:text-white text-gray-900"></i>
-            <p className="dark:text-white text-gray-900 text-sm">
-              Yangi karta qo'shing
-            </p>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+                        children,
+                        selected,
+                        ...rest
+                    }: ComponentProps<"div"> & { selected?: boolean }) {
+    return (
+        <div
+            className={`${
+                selected ? "border-green-500" : "border-transparent"
+            } border-2 rounded md:inline-block block w-auto h-auto`}
+        >
+            <div
+                {...rest}
+                className="group cursor-pointer m-3 sm:w-1/2 h-auto  md:w-[400px] md:min-h-[250px]  p-6 relative bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+                {children ? (
+                    children
+                ) : (
+                    <div className="text-center pt-3 cursor-pointer">
+                        <i className="fa-solid fa-plus text-9xl dark:text-white text-gray-900"></i>
+                        <p className="dark:text-white text-gray-900 text-sm">
+                            Yangi karta qo'shing
+                        </p>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default CreditCard;
--- client/app/components/Navbar/index.tsx
+++ client/app/components/Navbar/index.tsx
@@ -18,29 +18,6 @@ function Navbar() {
               YouPay
             </span>
                     </a>
-                    {/*<button*/}
-                    {/*    data-collapse-toggle="navbar-default"*/}
-                    {/*    type="button"*/}
-                    {/*    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"*/}
-                    {/*    aria-controls="navbar-default"*/}
-                    {/*    aria-expanded="false"*/}
-                    {/*>*/}
-                    {/*    <span className="sr-only">Open main menu</span>*/}
-                    {/*    <svg*/}
-                    {/*        aria-hidden="true"*/}
-                    {/*        xmlns="http://www.w3.org/2000/svg"*/}
-                    {/*        fill="none"*/}
-                    {/*        viewBox="0 0 17 14"*/}
-                    {/*    >*/}
-                    {/*        <path*/}
-                    {/*            stroke="currentColor"*/}
-                    {/*            strokeLinecap="round"*/}
-                    {/*            strokeLinejoin="round"*/}
-                    {/*            strokeWidth={2}*/}
-                    {/*            d="M1 1h15M1 7h15M1 13h15"*/}
-                    {/*        />*/}
-                    {/*    </svg>*/}
-                    {/*</button>*/}
                     <div className="block w-auto" id="navbar-default">
                         <ul className="font-medium flex items-center p-0  border-gray-100 rounded-lg space-x-8 mt-0 border-0 bg-white  dark:bg-gray-900 dark:border-gray-700">
                             <li>
