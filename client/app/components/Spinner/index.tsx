27ed4bcd08e7bb71113bde6dd78fc6881a0a1971
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-12-07 16:53:00 +0500

    client: Spinner component added size

--- client/app/components/Spinner/index.tsx
+++ client/app/components/Spinner/index.tsx
@@ -1,8 +1,14 @@
-function Spinner(){
-    return <div role="status">
+import {ComponentProps} from "react";
+
+function Spinner({size = "8", ...props}: { size?: string } & ComponentProps<"div">) {
+    return <div role="status" {...props}>
         <svg
+            style={{
+                width: size + "em",
+                height: size + "em"
+            }}
             aria-hidden="true"
-            className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            className={`mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600`}
             viewBox="0 0 100 101"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -20,4 +26,5 @@ function Spinner(){
     </div>
 
 }
+
 export default Spinner
--- client/app/settings/page.tsx
+++ client/app/settings/page.tsx
@@ -1,34 +1,15 @@
 "use client";
-import BasicUserDetails from "@/app/settings/components/BasicUserDetails";
 import {useMutation, useQuery, useQueryClient} from "react-query";
 import {userInfoQuery} from "@/app/utils/queryFunctions";
 import Text from "../components/Text";
-import ContactSettings from "@/app/settings/components/ContactSettings";
-import SettingCard from "@/app/settings/components/SettingCard";
-import DefaultInput from "@/app/components/DefaultInput";
-import Card from "@/app/components/Card";
-import DefaultButton from "@/app/components/DefaultButton";
-import GradientButton from "@/app/components/GradientButton";
-import Link from "next/link";
 import {mutationFunc, submitForm} from "@/app/utils/mutationFunctions";
 import Toast from "@/app/components/Toast";
+import Spinner from "@/app/components/Spinner";
+import BasicUserDetails from "@/app/settings/components/BasicUserDetails";
+import ContactSettings from "@/app/settings/components/ContactSettings";
+import PasswordSettings from "@/app/settings/components/PasswordSettings";
+import SubmitSettings from "@/app/settings/components/SubmitSettings";
 
-function PasswordSettings() {
-    return (
-        <Card style={{margin: "20px auto"}}>
-            <div className="mb-4">
-                <Text size="text-md">Parolni almashtirish</Text>
-            </div>
-            <SettingCard>
-                <div className="w-full">
-                    <DefaultInput type="password" label="Joriy parol"/>
-                    <DefaultInput type="password" name="password" label="Yangi parol"/>
-                    <DefaultInput type="password" label="Qayta takrorlang"/>
-                </div>
-            </SettingCard>
-        </Card>
-    );
-}
 
 function SettingsPage() {
     const {data} = useQuery("user", userInfoQuery);
@@ -46,17 +27,12 @@ function SettingsPage() {
                         <ContactSettings details={data.result}/>
                         <br/>
                         <PasswordSettings/>
-                        <div>
-                            <DefaultButton style={{margin: "0 10px"}} type="submit">
-                                O'zgarishlarni saqlash
-                            </DefaultButton>
-                            <Link href="/">
-                                <GradientButton>Kartalar bo'limi</GradientButton>
-                            </Link>
-                        </div>
+                        <SubmitSettings/>
                     </form>
+
                 ) : (
-                    <p></p>
+                    <div className='w-full mt-24 flex justify-center'><Spinner
+                        size={'8'}/></div>
                 )}
             </div>
             <Toast/>
