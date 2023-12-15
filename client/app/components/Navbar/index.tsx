08b5e394023f340f65dd3a66c9ada472da11e370
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2023-12-15 13:37:59 +0500

    client: favicon.ico and Logo updated

--- client/app/components/Navbar/index.tsx
+++ client/app/components/Navbar/index.tsx
@@ -3,6 +3,7 @@ import DropdownWithAvatar from "@/app/components/DropdownWithAvatar";
 import GradientButton from "@/app/components/GradientButton";
 import ToggleTheme from "../ToggleTheme";
 import {useAuth} from "@/app/hooks/useAuth";
+import Link from "next/link";
 
 function Navbar() {
     const auth = useAuth()
@@ -10,17 +11,17 @@ function Navbar() {
         <>
             <nav className="bg-white  z-50 w-full border-b dark:border-b-gray-800 dark:bg-gray-900">
                 <div className="flex items-center justify-between mx-auto p-4">
-                    <a href="https://flowbite.com/" className="flex items-center">
+                    <Link href='/' className="flex items-center">
                         <img
-                            src="https://flowbite.com/docs/images/logo.svg"
+                            src="https://i.ibb.co/99Ky60R/image-1.png"
                             className="h-8 mr-3"
-                            alt="Flowbite Logo"
+                            alt="YouPay Logo"
                         />
                         <span
                             className="hidden md:inline self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               YouPay
             </span>
-                    </a>
+                    </Link>
                     <div className="block w-auto" id="navbar-default">
                         <ul className="font-medium flex items-center p-0  border-gray-100 rounded-lg space-x-8 mt-0 border-0 bg-white  dark:bg-gray-900 dark:border-gray-700">
                             {auth && (<>
